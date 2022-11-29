681e529f2c412ebd5edeb18ae62fae7950789003
jamiekim828 <jamiekim828@gmail.com>
2022-11-29 10:52:20 +0100

    display without style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Country from './components/Country';
 
 function App() {
-  return <div className="App"></div>;
+  const [country, setCountry] = useState([]);
+
+  const getCountry = async () => {
+    const result = await fetch(`https://restcountries.com/v3.1/all`);
+    const json = await result.json();
+    console.log(json);
+    setCountry(json);
+  };
+  useEffect(() => {
+    getCountry();
+  }, []);
+
+  const slicedArray = country.slice(0, 6);
+  return (
+    <div className='App'>
+      {slicedArray.map((c, i) => (
+        <Country
+          key={i}
+          name={c.name.official}
+          region={c.region}
+          map={c.maps.googleMaps}
+          flag={c.flag}
+          borders={c.borders}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React from 'react';
 
-export default function Country() {
-  return <div>This is country </div>;
+export default function Country({ name, region, map, flag, borders }) {
+  return (
+    <div>
+      <h1>Name: {name}</h1>
+      <p>Region: {region}</p>
+      <h2>{flag}</h2>
+      <a href={`${map}`} target='_blank' rel='noreferrer'>
+        Visit map here
+      </a>
+      <ul>{borders ? borders.map((b, i) => <li key={i}>{b}</li>) : null}</ul>
+    </div>
+  );
 }
